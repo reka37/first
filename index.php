67d32262540991ffd37c64e35bca35ac31e5fa10
reka37 <reka37@mail.ru>
2021-10-06 18:01:37 +0300

    last

--- index.php
+++ index.php
@@ -4,11 +4,6 @@
 include 'class.php';
 
 
-<<<<<<< HEAD
-$testget = new Testgit('first odject -------------+++++++++++++sdsdsdsds');
+$testget = new Testgit('555');
 
-=======
-$testget = new Testgit('first odject -------------+++++++++++++uiuiuiuiuiui2323232323trtrtrt');
-echo 'dddrtrtrt';
->>>>>>> 479550b9d1ca9a75b754eb36249b1e232578c8dd
 ?>
