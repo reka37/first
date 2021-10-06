479550b9d1ca9a75b754eb36249b1e232578c8dd
Igor <reka37@mail.ru>
2021-10-06 17:47:59 +0300

    test123

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 include 'class.php';
 
 
-$testget = new Testgit('first odject -------------+++++++++++++uiuiuiuiuiui2323232323');
-echo 'ddd';
+$testget = new Testgit('first odject -------------+++++++++++++uiuiuiuiuiui2323232323trtrtrt');
+echo 'dddrtrtrt';
 ?>
