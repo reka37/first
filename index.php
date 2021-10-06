9920ec665e9481f46797cf0f422c1b07f1c5c52e
Igor <reka37@mail.ru>
2021-10-06 17:18:28 +0300

    firstnext

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 include 'class.php';
 
 
-$testget = new Testgit('first odject -------------+++++++++++++');
-
+$testget = new Testgit('first odject -------------+++++++++++++uiuiuiuiuiui2323232323');
+echo 'ddd';
 ?>
